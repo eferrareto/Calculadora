b22dc6b0d04df1e05de477f91f2bfd5d331e8474
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-08-24 09:07:17 -0300

    commit

--- calcular.js
+++ calcular.js
@@ -1,3 +1,13 @@
+
 function mensagem(numero){
-    document.getElementById('texto').innerHTML +=`${numero}`
+    var dados = document.getElementById('texto').innerHTML;
+    document.getElementById('texto').innerHTML = dados + numero  
+}
+function calcular(){
+    var resultado = document.getElementById('texto').innerHTML;
+    if (resultado){
+        document.getElementById('texto').innerHTML = eval(resultado)
+    } else {
+        document.getElementById('texto').innerHTML = ''
+    }
 }
